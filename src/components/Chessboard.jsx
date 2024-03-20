080685489bdaadeadbeaccf6094627697dc8f639
GOKULA KANNAN V <gokulakannanv04@gmail.com>
2024-03-20 07:38:27 +0530

    Automated commit - 2024-03-20 07:38:27

--- src/components/Chessboard.jsx
+++ src/components/Chessboard.jsx
@@ -1,42 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
+import canMove from './canMove'; // Import the canMove function
 import '../components/Chessboard.css'; // Import the CSS file
 
 // Define the initial board configuration
 const INITIAL_BOARD = [
-  ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook'],
-  ['pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn'],
-  [null, null, null, null, null, null, null, null],
-  [null, null, null, null, null, null, null, null],
-  [null, null, null, null, null, null, null, null],
-  [null, null, null, null, null, null, null, null],
-  ['pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn'],
-  ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook'],
+  ['brook', 'bknight', 'bbishop', 'bqueen', 'bking', 'bbishop', 'bknight', 'brook'],
+  ['bpawn', 'bpawn', 'bpawn', 'bpawn', 'bpawn', 'bpawn', 'bpawn', 'bpawn'],    
+  ['Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty'],
+  ['Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty'],
+  ['Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty'],
+  ['Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty'],
+  ['wpawn', 'wpawn', 'wpawn', 'wpawn', 'wpawn', 'wpawn', 'wpawn', 'wpawn'],   
+  ['wrook', 'wknight', 'wbishop', 'wqueen', 'wking', 'wbishop', 'wknight', 'wrook'],
 ];
 
 const Chessboard = () => {
-  const renderSquare = (piece, rowIndex, colIndex) => {
-    let pieceImage = null;
-    if (piece) {
-      const color = piece === piece.toUpperCase() ? 'w' : 'b'; // Check if piece is uppercase
-      pieceImage = require(`../images/${color}${piece.toLowerCase()}.png`).default;
+  const [board, setBoard] = useState(INITIAL_BOARD);
+  const [selectedPiece, setSelectedPiece] = useState(null);
+
+  const handleSquareClick = (row, col) => {
+    const piece = board[row][col];
+
+
+    if (selectedPiece === null) {
+      if (piece !== 'Empty') {
+        setSelectedPiece({ piece, row, col });
+        
+      }
+    } else {
+      console.log('Selected piece:', selectedPiece);
+
+      const { piece: selectedPieceName, row: selectedRow, col: selectedCol } = selectedPiece;
+      if (canMove(selectedPieceName, selectedRow, selectedCol, row, col, board)) { // Pass the board as an argument
+
+
+        const newBoard = [...board];
+        newBoard[row][col] = selectedPieceName;
+        newBoard[selectedRow][selectedCol] = 'Empty';
+        setBoard(newBoard);
+      }
+      setSelectedPiece(null);
     }
-    return <Square key={`${rowIndex}-${colIndex}`} piece={pieceImage} />;
+  };
+
+  const renderSquare = (piece, rowIndex, colIndex) => {
+    return (
+      <Square 
+        key={`${rowIndex}-${colIndex}`} 
+        piece={piece} 
+        onClick={() => handleSquareClick(rowIndex, colIndex)} 
+      />
+    );
   };
 
   const renderBoard = () => {
-    return INITIAL_BOARD.map((row, rowIndex) => (
+    return board.map((row, rowIndex) => (
       <div key={`row-${rowIndex}`} className="board-row">
         {row.map((piece, colIndex) => renderSquare(piece, rowIndex, colIndex))}
       </div>
     ));
   };
 
-  return (
-    <div className="chessboard">
-      {renderBoard()}
-    </div>
-  );
+  return <div className="chessboard">{renderBoard()}</div>;
 };
 
 export default Chessboard;
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import bknightImage from '../images/bknight.png';
+import bbishopImage from '../images/bbishop.png';
+import bqueenImage from '../images/bqueen.png';
+import bkingImage from '../images/bking.png';
+import bpawnImage from '../images/bpawn.png';
+import brookImage from '../images/brook.png';
+import wbishopImage from '../images/wbishop.png';
+import wknightImage from '../images/wknight.png';
+import wrookImage from '../images/wrook.png';
+import wqueenImage from '../images/wqueen.png';
+import wkingImage from '../images/wking.png';
+import wpawnImage from '../images/wpawn.png';
+const pieceImages = {
+  // Mapping piece names to their respective image components
+  'bknight': bknightImage,
+  'bbishop': bbishopImage,
+  'bqueen': bqueenImage,
+  'bking': bkingImage,
+  'bpawn': bpawnImage,
+  'brook': brookImage,
+  'wknight': wknightImage,
+  'wbishop': wbishopImage,
+  'wrook': wrookImage,
+  'wqueen': wqueenImage,
+  'wking': wkingImage,
+  'wpawn': wpawnImage,
+};
 
-const Square = ({ piece }) => {
+const Square = ({ piece, onClick }) => {
+  const piecei = pieceImages[piece];
   return (
-    <div className="square">
-      {piece && <img src={piece} alt="Piece" style={{ maxWidth: '100%', maxHeight: '100%' }} />}
+    <div className="square" onClick={onClick}>
+      {piece !== 'Empty' && <img src={piecei} alt={piece} style={{ maxWidth: '100%', maxHeight: '100%' }} />}
     </div>
   );
 };
