b47708fd723127199d0b64c16921ff9708528928
GOKULA KANNAN V <99313546+gokulakannanv04@users.noreply.github.com>
2024-03-20 16:11:10 +0530

    Update canMove.js

--- src/components/canMove.js
+++ src/components/canMove.js
@@ -12,22 +12,22 @@ const canMove = (piece, fromRow, fromCol, toRow, toCol, board) => {
       return canMoveBlackKnight(fromRow, fromCol, toRow, toCol);
     case 'wknight':
       return canMoveWhiteKnight(fromRow, fromCol, toRow, toCol);
-    // case 'bbishop':
-    //   return canMoveBlackBishop(fromRow, fromCol, toRow, toCol, board);
-    // case 'wbishop':
-    //   return canMoveWhiteBishop(fromRow, fromCol, toRow, toCol, board);
-    // case 'brook':
-    //   return canMoveBlackRook(fromRow, fromCol, toRow, toCol, board);
-    // case 'wrook':
-    //   return canMoveWhiteRook(fromRow, fromCol, toRow, toCol, board);
-    // case 'bqueen':
-    //   return canMoveBlackQueen(fromRow, fromCol, toRow, toCol, board);
-    // case 'wqueen':
-    //   return canMoveWhiteQueen(fromRow, fromCol, toRow, toCol, board);
-    // case 'bking':
-    //   return canMoveBlackKing(fromRow, fromCol, toRow, toCol, board);
-    // case 'wking':
-    //   return canMoveWhiteKing(fromRow, fromCol, toRow, toCol, board);
+    case 'bbishop':
+      return canMoveBlackBishop(fromRow, fromCol, toRow, toCol, board);
+    case 'wbishop':
+      return canMoveWhiteBishop(fromRow, fromCol, toRow, toCol, board);
+    case 'brook':
+      return canMoveBlackRook(fromRow, fromCol, toRow, toCol, board);
+    case 'wrook':
+      return canMoveWhiteRook(fromRow, fromCol, toRow, toCol, board);
+    case 'bqueen':
+      return canMoveBlackQueen(fromRow, fromCol, toRow, toCol, board);
+    case 'wqueen':
+      return canMoveWhiteQueen(fromRow, fromCol, toRow, toCol, board);
+    case 'bking':
+      return canMoveBlackKing(fromRow, fromCol, toRow, toCol, board);
+    case 'wking':
+      return canMoveWhiteKing(fromRow, fromCol, toRow, toCol, board);
     default:
       return false; // Invalid piece
   }
@@ -79,6 +79,118 @@ const canMoveBlackKnight = (fromRow, fromCol, toRow, toCol) => {
   // Knight moves in "L" shape: two squares in one direction and one square perpendicular to that direction
   return (Math.abs(toRow - fromRow) === 2 && Math.abs(toCol - fromCol) === 1) ||
          (Math.abs(toRow - fromRow) === 1 && Math.abs(toCol - fromCol) === 2);
+};
+// Function to determine if a black bishop can move from one position to another
+const canMoveBlackBishop = (fromRow, fromCol, toRow, toCol, board) => {
+  // Bishop moves diagonally any number of squares
+  if (Math.abs(toRow - fromRow) !== Math.abs(toCol - fromCol)) {
+    return false;
+  }
+  const rowIncrement = toRow > fromRow ? 1 : -1;
+  const colIncrement = toCol > fromCol ? 1 : -1;
+  let row = fromRow + rowIncrement;
+  let col = fromCol + colIncrement;
+  while (row !== toRow && col !== toCol) {
+    if (board[row][col] !== 'Empty') {
+      return false; // Path is blocked
+    }
+    row += rowIncrement;
+    col += colIncrement;
+  }
+  return true;
+};
+// Function to determine if a white bishop can move from one position to another
+const canMoveWhiteBishop = (fromRow, fromCol, toRow, toCol, board) => {
+  // Bishop moves diagonally any number of squares
+  if (Math.abs(toRow - fromRow) !== Math.abs(toCol - fromCol)) {
+    return false;
+  }
+  const rowIncrement = toRow > fromRow ? 1 : -1;
+  const colIncrement = toCol > fromCol ? 1 : -1;
+  let row = fromRow + rowIncrement;
+  let col = fromCol + colIncrement;
+  while (row !== toRow && col !== toCol) {
+    if (board[row][col] !== 'Empty') {
+      return false; // Path is blocked
+    }
+    row += rowIncrement;
+    col += colIncrement;
+  }
+  return true;
+};
+// Function to determine if a black rook can move from one position to another
+const canMoveBlackRook = (fromRow, fromCol, toRow, toCol, board) => {
+  // Rook moves horizontally or vertically any number of squares
+  if (fromRow !== toRow && fromCol !== toCol) {
+    return false;
+  }
+  if (fromRow === toRow) {
+    const colIncrement = toCol > fromCol ? 1 : -1;
+    let col = fromCol + colIncrement;
+    while (col !== toCol) {
+      if (board[fromRow][col] !== 'Empty') {
+        return false; // Path is blocked
+      }
+      col += colIncrement;
+    }
+  } else {
+    const rowIncrement = toRow > fromRow ? 1 : -1;
+    let row = fromRow + rowIncrement;
+    while (row !== toRow) {
+      if (board[row][fromCol] !== 'Empty') {
+        return false; // Path is blocked
+      }
+      row += rowIncrement;
+    }
+  }
+  return true;
+};
+// Function to determine if a white rook can move from one position to another
+const canMoveWhiteRook = (fromRow, fromCol, toRow, toCol, board) => {
+  // Rook moves horizontally or vertically any number of squares
+  if (fromRow !== toRow && fromCol !== toCol) {
+    return false;
+  }
+  if (fromRow === toRow) {
+    const colIncrement = toCol > fromCol ? 1 : -1;
+    let col = fromCol + colIncrement;
+    while (col !== toCol) {
+      if (board[fromRow][col] !== 'Empty') {
+        return false; // Path is blocked
+      }
+      col += colIncrement;
+    }
+  } else {
+    const rowIncrement = toRow > fromRow ? 1 : -1;
+    let row = fromRow + rowIncrement;
+    while (row !== toRow) {
+      if (board[row][fromCol] !== 'Empty') {
+        return false; // Path is blocked
+      }
+      row += rowIncrement;
+    }
+  }
+  return true;
+};
+// Function to determine if a black queen can move from one position to another
+const canMoveBlackQueen = (fromRow, fromCol, toRow, toCol, board) => {
+  // Queen combines the movement of rooks and bishops: can move horizontally, vertically, or diagonally any number of squares
+  return canMoveBlackRook(fromRow, fromCol, toRow, toCol, board) || canMoveBlackBishop(fromRow, fromCol, toRow, toCol, board);
+};
+// Function to determine if a white queen can move from one position to another
+const canMoveWhiteQueen = (fromRow, fromCol, toRow, toCol, board) => {
+  // Queen combines the movement of rooks and bishops: can move horizontally, vertically, or diagonally any number of squares
+  return canMoveWhiteRook(fromRow, fromCol, toRow, toCol, board) || canMoveWhiteBishop(fromRow, fromCol, toRow, toCol, board);
+};
+// Function to determine if a black king can move from one position to another
+const canMoveBlackKing = (fromRow, fromCol, toRow, toCol, board) => {
+  // King can move one square in any direction
+  return Math.abs(toRow - fromRow) <= 1 && Math.abs(toCol - fromCol) <= 1;
+};
+// Function to determine if a white king can move from one position to another
+const canMoveWhiteKing = (fromRow, fromCol, toRow, toCol, board) => {
+  // King can move one square in any direction
+  return Math.abs(toRow - fromRow) <= 1 && Math.abs(toCol - fromCol) <= 1;
 };
 
 // Add similar functions for other pieces...
