187f919e9442d303417c359b0ed7e7188caa07ee
GOKULA KANNAN V <gokulakannanv04@gmail.com>
2024-03-22 07:43:45 +0530

    Automated commit - 2024-03-22 07:43:45

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
+// // src/App.js
+// import React from 'react';
+// import './App.css';
+// import Chessboard from './components/Chessboard';
+// // import { INITIAL_BOARD } from './components/Chessboard';
+
+// function App() {
+  
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         {/* <h1>Chess Game</h1> */}
+//         <Chessboard  />
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
 import React from 'react';
 import './App.css';
 import P2PChat from './P2PChat'; // Import the P2PChat component
